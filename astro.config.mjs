a7388b0f8ff676f8d5c3f5c102a035e57b1ab824
Jason Paschall <jpaschall@gmail.com>
2025-07-06 16:53:33 -0500

    chore: remove @astrojs/cloudflare adapter and switch to default for Pages Functions support

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,8 +15,6 @@ export default defineConfig({
       }
     })
   ],
-  output: 'server',
-  adapter: cloudflare(),
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto',
