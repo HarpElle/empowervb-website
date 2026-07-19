1024c875cf5c90e8c1a1153336aff1dba435622b
Jason Paschall <jpaschall@gmail.com>
2026-07-19 04:06:29 -0500

    Harden EmpowerVB dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://empowervb.com',
   integrations: [
-    tailwind(),
     sitemap({
       filter: (page) => {
         // Exclude news-future page from sitemap
@@ -22,6 +20,7 @@ export default defineConfig({
   vite: {
     build: {
       cssCodeSplit: false,
+      cssMinify: 'esbuild',
     },
   },
 });
